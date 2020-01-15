3668ee2fff6b5d663233ea40cbd52247fe5c6df4
draro <58738792+draro@users.noreply.github.com>
2020-01-15 07:12:53 -0500

    Update disable_commands.py
    added check to the root user

--- disable_commands.py
+++ disable_commands.py
@@ -12,30 +12,30 @@ LOGGING = {
     'formatters': {
         'verbose': {
             'format': '%(levelname)s Module: %(module)s Process: %(process)d Thread: %(thread)d Message:%(message)s'
-            },
         },
+    },
     'handlers': {
         'stdout': {
             'class': 'logging.StreamHandler',
             'stream': sys.stdout,
             'level': logging.DEBUG,
             'formatter': 'verbose',
-            },
+        },
         'sys-logger6': {
             'class': 'logging.handlers.SysLogHandler',
             'address': '/dev/log',
             'facility': "local6",
             'level': logging.INFO,
             'formatter': 'verbose',
-            },
         },
+    },
     'loggers': {
         'user-commands': {
-            'handlers': ['stdout','sys-logger6'],
+            'handlers': ['stdout', 'sys-logger6'],
             'propagate': True,
-            },
-        }
+        },
     }
+}
 
 config.dictConfig(LOGGING)
 
@@ -43,7 +43,8 @@ config.dictConfig(LOGGING)
 logger = logging.getLogger("user-commands")
 
 
-groups = ['svn', 'wheel', 'admin', 'sys_admin','network_admin', 'security_admin', 'test2', r'test$']
+groups = ['svn', 'wheel', 'admin', 'sys_admin',
+          'network_admin', 'security_admin', 'test2', r'test$']
 
 
 def commands_config(user, group):
@@ -55,7 +56,7 @@ def commands_config(user, group):
     After found the file the program will write a bashrc.done_{group} and use that file to aplly the 
     rules in the user .bashrc file
     '''
-    logger.debug('{0},{1}'.format(user,group))
+    logger.debug('{0},{1}'.format(user, group))
     global groups
     if group in groups:
         logger.debug('{0},{1}'.format(group, groups))
@@ -65,7 +66,8 @@ def commands_config(user, group):
         if os.path.isfile('/home/{0}/.bashrc.original'.format(user)):
             logger.debug('/home/{0}/.bashrc.original Exists'.format(user))
         else:
-            os.system('cp /home/{0}/.bashrc /home/{0}/.bashrc.original'.format(user))
+            os.system(
+                'cp /home/{0}/.bashrc /home/{0}/.bashrc.original'.format(user))
             logger.debug('/home/{0}/.bashrc.original Created')
         try:
             if os.path.getsize(commands) > 0:
@@ -74,7 +76,8 @@ def commands_config(user, group):
                         with open(commands, mode='r+') as file:
                             rows = file.read().splitlines()
                             file.seek(0)
-                            bashrc = '/home/{0}/.bashrc.done_{1}'.format(user, group)
+                            bashrc = '/home/{0}/.bashrc.done_{1}'.format(
+                                user, group)
                             bashrc_orig = '/home/{0}/.bashrc'.format(user)
                             try:
                                 if os.path.isfile(bashrc):
@@ -86,10 +89,12 @@ def commands_config(user, group):
                                             with open(bashrc_orig, mode='r') as orig:
                                                 file = orig.read()
                                                 if row in str(file):
-                                                    logger.debug('Row containing {0} already exist'.format(row))
+                                                    logger.debug(
+                                                        'Row containing {0} already exist'.format(row))
 
                                                 else:
-                                                    logger.debug('Row with {0} missing'.format(row))
+                                                    logger.debug(
+                                                        'Row with {0} missing'.format(row))
                                                     with open(bashrc_orig, mode='a')as original:
                                                         original.write(
                                                             'alias '+row+'='+'"echo ' + "'You are not allowed to run this command'"+'"'+'\n')
@@ -100,10 +105,13 @@ def commands_config(user, group):
                                             with open(bashrc, mode='r+') as done:
                                                 rows = done.read()
                                                 if line in rows:
-                                                    logger.debug('Row containing {0} already exist'.format(line))
+                                                    logger.debug(
+                                                        'Row containing {0} already exist'.format(line))
                                                 else:
-                                                    logger.debug('Row with {0} missing'.format(line))
-                                                    done.write('alias '+line+'='+'"echo ' + "'You are not allowed to run this command'"+'"'+'\n')
+                                                    logger.debug(
+                                                        'Row with {0} missing'.format(line))
+                                                    done.write(
+                                                        'alias '+line+'='+'"echo ' + "'You are not allowed to run this command'"+'"'+'\n')
                                                     with open(bashrc_orig, mode='a')as file:
                                                         file.write(
                                                             'alias '+line+'='+'"echo ' + "'You are not allowed to run this command'"+'"'+'\n')
@@ -116,20 +124,24 @@ def commands_config(user, group):
                                         'cat {0} >>/home/{1}/.bashrc '.format(bashrc, user))
 
                             except Exception as e:
-                                logger.exception('An Error Occurred: {0}'.format(e))
+                                logger.exception(
+                                    'An Error Occurred: {0}'.format(e))
                     else:
-                        logger.warning('File {0} not found'.format(back_bashrc))
+                        logger.warning(
+                            'File {0} not found'.format(back_bashrc))
                 except OSError as e:
                     logger.error('(!) Unable to get file size: {}'.format(e))
             else:
                 try:
                     if os.path.getsize(commands) > 0 and os.path.isfile(back_bashrc):
-                        os.system('cp {0} /home/{1}/.bashrc'.format(back_bashrc,user))
+                        os.system(
+                            'cp {0} /home/{1}/.bashrc'.format(back_bashrc, user))
                         logger.debug('Apllying the original bashrc')
                 except OSError as e:
                     logger.info('(!) Unable to get file size: {}'.format(e))
                 logger.warning('File {0} not found'.format(commands))
-                logger.warning("User {0} does not have command's limitation".format(user))
+                logger.warning(
+                    "User {0} does not have command's limitation".format(user))
         except OSError as e:
             logger.info('(!) Unable to get file size: {}'.format(e))
         except Exception as e:
@@ -149,11 +161,16 @@ def get_users_gid():
                 f = file.read()
                 for group in groups:
                     if group in f:
-                        logger.debug('Found group: ' + group + ' for user: ' + user)
+                        logger.debug('Found group: ' + group +
+                                     ' for user: ' + user)
                         commands_config(user, group)
+
 
 if __name__ == "__main__":
     try:
+        if os.system('whoami') != 'root':
+            logger.exception("User Must be ROOT")
+            exit(1)
         get_users_gid()
     except Exception:
         logger.exception("Exception in get_users_gid(): ")
