f86d569aa222a4e5b946f79ebf9cc93f3a53e1ae
draro <58738792+draro@users.noreply.github.com>
2020-01-16 10:56:45 -0500

    Update disable_commands.py

--- disable_commands.py
+++ disable_commands.py
@@ -5,6 +5,19 @@ import pdb
 import sys
 import fnmatch
 from logging import config
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--enable', action='store_true',
+                    dest='e', help="Enable command")
+parser.add_argument('-u', '--userr', type=str,
+                    dest='u', help="define the user")
+parser.add_argument('-c', '--command', dest='c',
+                    type=str, help="define the command to enable")
+
+args = parser.parse_args()
+
 
 LOGGING = {
     'version': 1,
@@ -32,19 +45,16 @@ LOGGING = {
     'loggers': {
         'user-commands': {
             'handlers': ['stdout', 'sys-logger6'],
-            'level': logging.DEBUG,
+            'level': logging.INFO,
             'propagate': True,
         },
     }
 }
 
 config.dictConfig(LOGGING)
-
-
 logger = logging.getLogger("user-commands")
-
-
-groups = ['svn', 'wheel', 'admin', 'sys_admin',
+# GROUPS TO BE CHANGED DEPENDING ON THE EXPECTED GROUP ON LDAP
+groups = ['svn', 'wheel', 'admin', 'sys_admin', 'test2',
           'network_admin', 'security_admin']
 
 
@@ -64,6 +74,9 @@ def commands_config(user, group):
         commands = '/tmp/commands_'+group
         logger.debug(commands)
         back_bashrc = '/home/{0}/.bashrc.original'.format(user)
+        bashrc = '/home/{0}/.bashrc.done_{1}'.format(user, group)
+        bashrc_orig = '/home/{0}/.bashrc'.format(user)
+        # CREATE BASHRC BACKUP IF NOT EXISTS
         if os.path.isfile('/home/{0}/.bashrc.original'.format(user)):
             logger.debug('/home/{0}/.bashrc.original Exists'.format(user))
         else:
@@ -71,15 +84,14 @@ def commands_config(user, group):
                 'cp /home/{0}/.bashrc /home/{0}/.bashrc.original'.format(user))
             logger.debug('/home/{0}/.bashrc.original Created')
         try:
+            # CHECK that /tmp/commands_{group} is not empty
             if os.path.getsize(commands) > 0:
                 try:
+                    # check bashrc.original is not empty
                     if os.path.getsize(back_bashrc) > 0:
                         with open(commands, mode='r+') as file:
                             rows = file.read().splitlines()
                             file.seek(0)
-                            bashrc = '/home/{0}/.bashrc.done_{1}'.format(
-                                user, group)
-                            bashrc_orig = '/home/{0}/.bashrc'.format(user)
                             try:
                                 if os.path.isfile(bashrc):
                                     logger.debug('{0} Exists'.format(bashrc))
@@ -124,9 +136,10 @@ def commands_config(user, group):
                                                         file.write(
                                                             'alias '+line+'='+'"echo ' + "'You are not allowed to run this command'"+'"'+'\n')
                                 else:
-                                    with open(bashrc, mode='a')as file:
-                                        file.write(
-                                            'alias '+row+'='+'"echo ' + "'You are not allowed to run this command'"+'"'+'\n')
+                                    for row in rows:
+                                        with open(bashrc, mode='a')as file:
+                                            file.write(
+                                                'alias '+row+'='+'"echo ' + "'You are not allowed to run this command'"+'"'+'\n')
                                     with open(bashrc, mode='r+') as done:
                                         rows = done.read().splitlines()
                                         done.seek(0)
@@ -176,12 +189,24 @@ def commands_config(user, group):
                             'File {0} not found'.format(back_bashrc))
                 except OSError as e:
                     logger.error('(!) Unable to get file size: {}'.format(e))
+                logger.error('i am line 177')
+                # for fileList in os.listdir('/home/{0}/'.format(user)):
+                #     if fileList.startswith('.bashrc.done'):
+                #         logger.error('{0}'.format(fileList))
+
             else:
                 try:
+                    os.remove(bashrc)
+                    logger.critical('removed bashrc.done')
                     if os.path.getsize(commands) == 0 & os.path.isfile(back_bashrc):
-                        os.system(
-                            'cp {0} /home/{1}/.bashrc'.format(back_bashrc, user))
-                        logger.info('Apllying the original bashrc')
+                        file = []
+                        for files in os.listdir('/home/{0}'.format(user)):
+                            if files.startswith('.baschrc.done_'):
+                                file.append(files)
+                        if len(file) == 1:
+                            os.system(
+                                'cp {0} /home/{1}/.bashrc'.format(back_bashrc, user))
+                            logger.info('Apllying the original bashrc')
                     else:
                         logger.warning('File {0} not found'.format(commands))
                         logger.warning(
@@ -194,6 +219,8 @@ def commands_config(user, group):
             logger.info('(!) Unable to get file size: {}'.format(e))
         except Exception as e:
             logger.exception('An Error Occurred: {0}'.format(e))
+    else:
+        logger.info('{0} not in the coded groups'.format(user))
 
 
 def get_users_gid():
@@ -214,12 +241,47 @@ def get_users_gid():
                         commands_config(user, group)
 
 
+def enamble_command(u, c):
+    logger.info('ENABLE COMMAND FUNCTION')
+    command = c
+    user = u
+    logger.info('Enabling the command "{0}" for {1}'.format(command, user))
+    for dirName, subDir, fileNames in os.walk('/home/{0}'.format(user)):
+        for f in fileNames:
+            if f.startswith('.bashrc'):
+                with open("/home/{0}/{1}".format(user, f), "r+") as file:
+                    logger.debug('opening /home/{0}/{1}'.format(user, f))
+                    d = file.readlines()
+                    file.seek(0)
+                    for i in d:
+                        if not command in i:
+                            logger.debug('{0} not in {1}'.format(command, i))
+                            file.write(i)
+                        else:
+                            logger.debug('{0} in {1}'.format(command, i))
+                    file.truncate()
+
+
 if __name__ == "__main__":
     try:
         if os.geteuid() != 0:
             logger.critical("User Must be ROOT!")
             exit(1)
-        get_users_gid()
+        if args.e and args.c and args.u:
+            enamble_command(args.u, args.c)
+        elif args.e and args.c and not args.u:
+            logger.critical("the option -u shall be used")
+        elif args.e and not args.c and args.u:
+            logger.critical("the option -c shall be used")
+        elif not args.e and not args.c and args.u:
+            logger.critical("Missing arguments")
+        elif not args.e and args.c and args.u:
+            logger.critical("Argument --enable required")
+            print("Argument --enable required")
+        elif not args.e and args.c and not args.u:
+            logger.critical("Missing arguments")
+        else:
+            get_users_gid()
     except Exception as e:
-        logger.exception("Exception in get_users_gid(): {0}".format(e))
+        logger.exception("Exception: {0}".format(e))
         exit(1)
